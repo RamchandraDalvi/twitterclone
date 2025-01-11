e580f6e09b1663375407f05dd193ac51a4305930
Ramchandra <rdalvi1210@gmail.com>
2025-01-11 20:28:19 +0530

    new updates again

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(urlencoded({ extended: true }));
 const corsOptions = {
-    origin: process.env.URL,
+    origin: "https://twitterclone-1-g05t.onrender.com",
     credentials: true
 }
 app.use(cors(corsOptions));
